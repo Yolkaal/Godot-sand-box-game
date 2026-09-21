5afcc6fdd0ee00f88c5f1b3f2458940e90abca43
Angelo Gonzalez <alpharogue71@gmail.com>
2026-09-21 15:28:01 +0200

    fix : movement system + feat : first person camera

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -17,7 +17,7 @@ public partial class Player : CharacterBody3D
     public int FallAcceleration { get; set; } = 75;
 
     [Export]
-    public float mouse_sensitivity = 0.002f;
+    public float MouseSensitivity = 0.002f;
 
     #endregion
 
@@ -27,16 +27,33 @@ public partial class Player : CharacterBody3D
     #region Private fields
 
     private Vector3 _targetVelocity = Vector3.Zero;
+    private float _pitch = 0f;
+    private Node3D _camera;
+    private Node3D _yaw;
 
     #endregion
 
 
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            RotateY(-mouseMotion.Relative.X * MouseSensitivity);
 
+            _pitch -= mouseMotion.Relative.Y * MouseSensitivity;
+            _pitch = Mathf.Clamp(_pitch, Mathf.DegToRad(-89f), Mathf.DegToRad(89f));
+            _camera.Rotation = new Vector3(_pitch, 0f, 0f);
+        }
+    }
 
 
     public override void _Ready()
     {
         GD.Print("Player initialized");
+        _yaw = GetNode<Node3D>("Pivot");
+        _camera = GetNode<Camera3D>("Pivot/Camera3D");
+
+        Input.MouseMode = Input.MouseModeEnum.Captured;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -62,7 +79,7 @@ public partial class Player : CharacterBody3D
 
         if (direction != Vector3.Zero)
         {
-            direction = direction.Normalized();
+            direction = Transform.Basis * direction.Normalized();
         }
 
         _targetVelocity.X = direction.X * MovementSpeed;
