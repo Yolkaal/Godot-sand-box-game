540a6ee016fb47ae491e558599eab378c080648f
Angelo Gonzalez <alpharogue71@gmail.com>
2026-09-21 17:51:58 +0200

    feat : jump system interface

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -2,6 +2,7 @@ namespace PlayerMovement;
 
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Player : CharacterBody3D
 {
@@ -14,15 +15,12 @@ public partial class Player : CharacterBody3D
     public int MovementSpeed { get; set; } = 5;
 
     [Export]
-    public int FallAcceleration { get; set; } = 75;
+    public int FallAcceleration { get; set; } = 35;
 
     [Export]
     public float MouseSensitivity = 0.002f;
 
     #endregion
-
-
-
 
     #region Private fields
 
@@ -30,11 +28,15 @@ public partial class Player : CharacterBody3D
     private float _pitch = 0f;
     private Node3D _camera;
     private Node3D _yaw;
+    private List<IMovementAbility> _abilities = [];
 
     #endregion
 
 
-    public override void _Input(InputEvent @event)
+
+
+    // # Perform Camera Rotation
+    private void PerformRotation(InputEvent @event)
     {
         if (@event is InputEventMouseMotion mouseMotion)
         {
@@ -46,17 +48,18 @@ public partial class Player : CharacterBody3D
         }
     }
 
-
-    public override void _Ready()
+    // # Perform Movement initialisation
+    private void InitializeCamera()
     {
-        GD.Print("Player initialized");
         _yaw = GetNode<Node3D>("Pivot");
         _camera = GetNode<Camera3D>("Pivot/Camera3D");
 
         Input.MouseMode = Input.MouseModeEnum.Captured;
+        GD.Print("Player Camera initialized");
     }
 
-    public override void _PhysicsProcess(double delta)
+    // # Get Player Movement Input
+    private Vector3 GetInput()
     {
         var direction = Vector3.Zero;
 
@@ -82,17 +85,59 @@ public partial class Player : CharacterBody3D
             direction = Transform.Basis * direction.Normalized();
         }
 
+        return direction;
+    }
+
+    // # Perform Player Movement
+    private void PerformMovement(double delta)
+    {
+        var direction = GetInput();
+
+
+
         _targetVelocity.X = direction.X * MovementSpeed;
         _targetVelocity.Z = direction.Z * MovementSpeed;
 
-        if (!IsOnFloor())
+        if (IsOnFloor() && _targetVelocity.Y < 0)
+            {
+                _targetVelocity.Y = -0.1f;
+            }
+            else if (!IsOnFloor())
+            {
+                _targetVelocity.Y -= FallAcceleration * (float)delta;
+            }
+
+        foreach (var ability in _abilities)
         {
-            _targetVelocity.Y -= FallAcceleration * (float)delta;
+            ability.PhysicsUpdate(delta, ref _targetVelocity, this);
         }
 
         Velocity = _targetVelocity;
         MoveAndSlide();
     }
 
+    private void InitializeAbilities()
+    {
+        _abilities.Add(new JumpAbility(jumpVelocity: 10f, maxJumps: 1));
+    }
 
+
+
+
+    // ? MAIN PROGRAM
+    public override void _Input(InputEvent @event)
+    {
+        PerformRotation(@event);
+    }
+
+    public override void _Ready()
+    {
+        InitializeCamera();
+        InitializeAbilities();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        PerformMovement(delta);
+    }
 }
