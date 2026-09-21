8cd7157886d461c8b0b09767f3b6324082e37f4e
Angelo Gonzalez <alpharogue71@gmail.com>
2026-09-21 14:47:25 +0200

    fin du système de déplacement

---
+++ scripts/Player.cs
@@ -0,0 +1,68 @@
+namespace PlayerMovement;
+
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+
+    # region Exported properties
+
+    [Export]
+    public int MovementSpeed { get; set; } = 5;
+
+    [Export]
+    public int FallAcceleration { get; set; } = 75;
+
+    #endregion
+
+    #region Private fields
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    #endregion
+
+    public override void _Ready()
+    {
+        GD.Print("Player initialized");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = Vector3.Zero;
+
+        if (Input.IsActionPressed("move_forward"))
+        {
+            direction.Z += 1.0f;
+        }
+        if (Input.IsActionPressed("move_back"))
+        {
+            direction.Z -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            direction.X -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            direction.X += 1.0f;
+        }
+
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+        }
+
+        _targetVelocity.X = direction.X * MovementSpeed;
+        _targetVelocity.Z = direction.Z * MovementSpeed;
+
+        if (!IsOnFloor())
+        {
+            _targetVelocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
+}
